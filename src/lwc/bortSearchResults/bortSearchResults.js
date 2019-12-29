055fcd72cf6119790d0f2fbe0fe27d426e3c64b2
MASA-JAPAN <masaifactoryline@gmail.com>
2019-12-29 16:21:14 +0900

    commit

--- src/lwc/bortSearchResults/bortSearchResults.js
+++ src/lwc/bortSearchResults/bortSearchResults.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-console */
 import { LightningElement, track, api } from "lwc";
 
 export default class BortSearchResults extends LightningElement {
   @track borts;
+  @track selectedBortId;
 
   @api
   set searchedBorts(value) {
--- src/lwc/bortTile/bortTile.js
+++ src/lwc/bortTile/bortTile.js
@@ -2,19 +2,29 @@
 import { LightningElement, track, api } from "lwc";
 
 export default class BortTile extends LightningElement {
-  @track bort = "{Picture__c: }";
+  @track bort;
+  @track bortId = "";
+  @track bortContactName = "";
+  @track bortPicture = "";
 
   @api
   set searchedBort(value) {
     console.log(value);
 
     this.bort = value;
+    this.bortContactName = value.Contact__r.Name;
+    this.bortPicture = value.Picture__c;
   }
   get searchedBort() {
     return this.bort;
   }
 
   get backgroundStyle() {
-    return `background-image:url(${this.bort.Picture__c})`;
+    return `background-image:url(${this.bortPicture})`;
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.dispatchEvent(new CustomEvent("selected", { detail: this.bort.Id }));
   }
 }
